11bf936f2efe84f3c4c06e57c9c48cab730acf76
M. Dietrich <mdt@emdete.de>
2015-03-30 17:28:25 +0200

    add missing Constants

---
+++ src/main/java/org/pyneo/maps/utils/Constants.java
@@ -0,0 +1,8 @@
+package org.pyneo.maps.utils;
+
+import org.andnav.osm.util.constants.OpenStreetMapConstants;
+import org.andnav.osm.views.util.constants.OpenStreetMapViewConstants;
+
+public interface Constants extends OpenStreetMapConstants, OpenStreetMapViewConstants {
+	static public final String LOGTAG = "org.pyneo.maps";
+}
