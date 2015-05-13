1df9d45ebf4d8e715e178af8e795ecd2b363b2c4
M. Dietrich <mdt@emdete.de>
2015-05-13 14:35:29 +0200

    reorder constants

--- src/main/java/org/pyneo/maps/map/Constants.java
+++ src/main/java/org/pyneo/maps/map/Constants.java
@@ -1,7 +1,4 @@
 package org.pyneo.maps.map;
 
 public interface Constants extends org.pyneo.maps.Constants {
-	public static final int ZOOM_CONTROL_HIDE = 0;
-	public static final int ZOOM_CONTROL_TOP = 1;
-	public static final int ZOOM_CONTROL_BOTTOM = 2;
 }
--- src/main/java/org/pyneo/maps/track/DatabaseHelper.java
+++ src/main/java/org/pyneo/maps/track/DatabaseHelper.java
@@ -4,21 +4,17 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import org.pyneo.maps.poi.Constants;
-
-public class DatabaseHelper extends SQLiteOpenHelper {
-
+public class DatabaseHelper extends SQLiteOpenHelper implements Constants {
 	public DatabaseHelper(Context context, String name) {
 		super(context, name, null, 1);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL(Constants.SQL_CREATE_trackpoints);
+		db.execSQL(SQL_CREATE_trackpoints);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 	}
-
 }
